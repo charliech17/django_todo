7e3f56e6ad9ce619a48ae2a23a5515a2083a83d3
Josh <kcchang1994@gmail.com>
2024-12-21 16:56:51 +0800

    fix render deploy error

--- build.sh
+++ build.sh
@@ -3,9 +3,7 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install pipenv
-pipenv install
-pipenv shell
+pip install -r requirements.txt
 
 # Convert static asset files
 python manage.py collectstatic --no-input
